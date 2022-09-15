2a646c4c8a351bd8e6b86ccc214129ed43807e38
searchableguy <73341821+searchableguy@users.noreply.github.com>
2022-09-15 14:57:53 +0530

    feat: Add algolia and replace local docs search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,91 +16,122 @@ const config = {
   favicon: "img/favicon.ico",
   organizationName: "unlock-protocol", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
-  plugins: [require.resolve("@cmfcmf/docusaurus-search-local"),
-  [
-    require.resolve('docusaurus-gtm-plugin'),
-    {
-      id: 'GTM-PRCCFV9', // GTM Container ID
-    }
+
+  plugins: [
+    [
+      require.resolve("docusaurus-gtm-plugin"),
+      {
+        id: "GTM-PRCCFV9", // GTM Container ID
+      },
+    ],
+    [
+      "@docusaurus/plugin-client-redirects",
+      {
+        redirects: [
+          {
+            from: "/unlock",
+            to: "/",
+          },
+          {
+            from: "/basics",
+            to: "/",
+          },
+          {
+            from: "/unlock/developers/locksmith/webhooks",
+            to: "/tools/locksmith/webhooks",
+          },
+          {
+            from: "/unlock/governance/roadmap",
+            to: "/governance/roadmap",
+          },
+          {
+            from: "/basics/new-to-unlock/unlock-accounts",
+            to: "/tools/sign-in-with-ethereum/unlock-accounts",
+          },
+          {
+            from: "/basics/unlock-accounts",
+            to: "/tools/sign-in-with-ethereum/unlock-accounts",
+          },
+          {
+            from: "/tools/paywall/sign-in-with-ethereum",
+            to: "/tools/sign-in-with-ethereum/",
+          },
+          {
+            from: "/new-to-web3/ethereum-architecture",
+            to: "/getting-started/new-to-web3/ethereum-architecture",
+          },
+          {
+            from: "/new-to-web3/",
+            to: "/getting-started/new-to-web3/",
+          },
+          {
+            from: "/new-to-web3/using-etherscan",
+            to: "/getting-started/new-to-web3/using-etherscan",
+          },
+          {
+            from: "/new-to-web3/what-is-a-crypto-wallet-a-guide-for-developers",
+            to: "/getting-started/new-to-web3/what-is-a-crypto-wallet",
+          },
+          {
+            from: "/new-to-web3/what-is-a-decentralised-application-aka-dapp",
+            to: "/getting-started/new-to-web3/what-is-a-decentralised-application-aka-dapp",
+          },
+          {
+            from: "/new-to-web3/what-is-a-smart-contract",
+            to: "/getting-started/new-to-web3/what-is-a-smart-contract",
+          },
+          {
+            from: "/new-to-web3/what-is-an-nft",
+            to: "/getting-started/new-to-web3/what-is-an-nft",
+          },
+          {
+            from: "/our-code-of-conduct",
+            to: "/getting-started/our-code-of-conduct",
+          },
+          {
+            from: "/litepaper",
+            to: "/getting-started/what-is-unlock/litepaper",
+          },
+          {
+            from: "/tutorials/backend-locking-with-express.js",
+            to: "/tutorials/back-end/backend-locking-with-express.js",
+          },
+          {
+            from: "/tutorials/ad-free-experience",
+            to: "/tutorials/front-end/ad-free-experience",
+          },
+          {
+            from: "/tutorials/locking-media-content",
+            to: "/tutorials/front-end/locking-media-content",
+          },
+          {
+            from: "/tutorials/react-example",
+            to: "/tutorials/front-end/react-example",
+          },
+          {
+            from: "/tutorials/using-subgraphs",
+            to: "/tutorials/misc/using-subgraphs",
+          },
+          {
+            from: "/tutorials/ethers",
+            to: "/tutorials/smart-contracts/ethers",
+          },
+          {
+            from: "/tutorials/the-key-purchase-hook",
+            to: "/tutorials/smart-contracts/the-key-purchase-hook",
+          },
+          {
+            from: "/tutorials/using-an-existing-nft-contract",
+            to: "/tutorials/smart-contracts/using-an-existing-nft-contract",
+          },
+          {
+            from: "/tutorials/using-unlock-in-other-contracts",
+            to: "/tutorials/smart-contracts/using-unlock-in-other-contracts",
+          },
+        ],
+      },
+    ],
   ],
-  ['@docusaurus/plugin-client-redirects', {
-    redirects: [{
-      from: '/unlock',
-      to: '/',
-    }, {
-      from: '/basics',
-      to: '/'
-    }, {
-      from: '/unlock/developers/locksmith/webhooks',
-      to: '/tools/locksmith/webhooks'
-    }, {
-      from: '/unlock/governance/roadmap',
-      to: '/governance/roadmap'
-    }, {
-      from: '/basics/new-to-unlock/unlock-accounts',
-      to: '/tools/sign-in-with-ethereum/unlock-accounts'
-    }, {
-      from: '/basics/unlock-accounts',
-      to: '/tools/sign-in-with-ethereum/unlock-accounts'
-    }, {
-      from: '/tools/paywall/sign-in-with-ethereum',
-      to: '/tools/sign-in-with-ethereum/'
-    },{
-      from: '/new-to-web3/ethereum-architecture',
-      to: '/getting-started/new-to-web3/ethereum-architecture'
-    },{
-      from: '/new-to-web3/',
-      to: '/getting-started/new-to-web3/'
-    },{
-      from: '/new-to-web3/using-etherscan',
-      to: '/getting-started/new-to-web3/using-etherscan'
-    },{
-      from: '/new-to-web3/what-is-a-crypto-wallet-a-guide-for-developers',
-      to: '/getting-started/new-to-web3/what-is-a-crypto-wallet'
-    },{
-      from: '/new-to-web3/what-is-a-decentralised-application-aka-dapp',
-      to: '/getting-started/new-to-web3/what-is-a-decentralised-application-aka-dapp'
-    },{
-      from: '/new-to-web3/what-is-a-smart-contract',
-      to: '/getting-started/new-to-web3/what-is-a-smart-contract'
-    },{
-      from: '/new-to-web3/what-is-an-nft',
-      to: '/getting-started/new-to-web3/what-is-an-nft'
-    },{
-      from: '/our-code-of-conduct',
-      to: '/getting-started/our-code-of-conduct'
-    },{
-      from: '/litepaper',
-      to: '/getting-started/what-is-unlock/litepaper'
-    },{
-      from: '/tutorials/backend-locking-with-express.js',
-      to: '/tutorials/back-end/backend-locking-with-express.js'
-    },{
-      from: '/tutorials/ad-free-experience',
-      to: '/tutorials/front-end/ad-free-experience'
-    },{
-      from: '/tutorials/locking-media-content',
-      to: '/tutorials/front-end/locking-media-content'
-    },{
-      from: '/tutorials/react-example',
-      to: '/tutorials/front-end/react-example'
-    },{
-      from: '/tutorials/using-subgraphs',
-      to: '/tutorials/misc/using-subgraphs'
-    },{
-      from: '/tutorials/ethers',
-      to: '/tutorials/smart-contracts/ethers'
-    },{
-      from: '/tutorials/the-key-purchase-hook',
-      to: '/tutorials/smart-contracts/the-key-purchase-hook'
-    },{
-      from: '/tutorials/using-an-existing-nft-contract',
-      to: '/tutorials/smart-contracts/using-an-existing-nft-contract'
-    },{
-      from: '/tutorials/using-unlock-in-other-contracts',
-      to: '/tutorials/smart-contracts/using-unlock-in-other-contracts'
-    }]
-  }]],
   presets: [
     [
       "docusaurus-preset-openapi",
@@ -123,166 +154,166 @@ const config = {
     ],
   ],
 
-  themeConfig:{
+  themeConfig: {
     metadata: [
-        { name: "keywords",
-          content: "unlock, blockchain, nft, token-gate, memberships, subscriptions"
-        },
-        { property: "og:locale",
-           content: "og:en_US"
-        },
-        { poperty: "og:type",
-          content: "website"
-        },
-        { property: "og:description",
-          content: "Unlock Protocol technical documentation for developers with a complete protocol reference, tutorials and code examples."
-        },
-        { property: "og:title",
-          content: "Unlock Protocol Technical Docs"
-        },
-        { property: "og:url",
-          content: "https://docs.unlock-protocol.com/"
-        },
-        { property: "og:image",
-          content:"/img/dev-docs-share-img.png"
-        },
-        { property: "og:image:width",
-          content: "1200"
-        },
-        { property: "og:image:height",
-          content: "627"
-        },
-        { property: "og:image:type",
-          content: "image/png"
-        },
-        { name: "twitter:card",
-          content: "summary_large_image"
-        },
-        { name: "twitter:title",
-          content: "Unlock Protocol Technical Docs"
-        },
-        { name: "twitter:description",
-          content: "Unlock Protocol technical documentation for developers with a complete protocol reference, tutorials and code examples."
-        },
-        { name: "twitter:image",
-          content: "/img/dev-docs-share-img.png"
-        },
-        { name: "twitter:image:alt",
-          content: "Unlock logo with the word docs next to it"
-        }
-      ],
-      navbar: {
-        title: "Unlock",
-        logo: {
-          alt: "Unlock Protocol",
-          src: "img/logo.svg",
-          href: "https://unlock-protocol.com/"
-        },
-        items: [
-          { to: "/", label: "Docs", position: "right" },
-          { to: "https://app.unlock-protocol.com/dashboard", label: "Dashboard", position: "right" },
-          { to: "/core-protocol", label: "Core Protocol", position: "right" },
-          { to: "/governance", label: "Governance", position: "right" },
-          {
-            href: "https://github.com/unlock-protocol/unlock",
-            label: "GitHub",
-            position: "right",
-          },
-          { to: 'https://unlock-protocol.gitbook.io/', label: "Older Docs", position: "right", rel: "nofollow,noindex" }
-        ],
+      {
+        name: "keywords",
+        content:
+          "unlock, blockchain, nft, token-gate, memberships, subscriptions",
       },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Overview",
-                to: "/",
-              },
-              {
-                label: "Tools",
-                to: "/",
-              },
-              {
-                label: "Tutorials",
-                to: "/",
-              },
-              {
-                label: "Goverance",
-                to: "/",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/unlock-protocol/unlock",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Discord",
-                href: "https://discord.com/invite/Ah6ZEJyTDp",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/UnlockProtocol",
-              },
-              {
-                label: "Forum",
-                href: "https://unlock.community/",
-              },
-            ],
-          },
-          {
-            title: "About Unlock",
-            items: [
-              {
-                label: "About Unlock",
-                to: "https://unlock-protocol.com",
-              },
-              {
-                label: "Blog",
-                to: "https://app.unlock-protocol.com",
-              },
-              {
-                label: "Guides",
-                to: "https://unlock-protocol.com/guides",
-              },
-              {
-                label: "Brand kit",
-                to: "https://unlock-protocol.com/guides/#",
-              },
-            ],
-          },
-          {
-            title: "Unlock apps",
-            items: [
-              {
-                label: "Launch dashboard",
-                to: "https://app.unlock-protocol.com",
-              },
-              {
-                label: "Grants for developer",
-                to: "https://unlock-protocol.com/grants",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Unlock, Inc.`,
+      { property: "og:locale", content: "og:en_US" },
+      { poperty: "og:type", content: "website" },
+      {
+        property: "og:description",
+        content:
+          "Unlock Protocol technical documentation for developers with a complete protocol reference, tutorials and code examples.",
       },
-      prism: {
-        additionalLanguages: ['solidity'],
-        theme: UnlockPrismTheme,
+      { property: "og:title", content: "Unlock Protocol Technical Docs" },
+      { property: "og:url", content: "https://docs.unlock-protocol.com/" },
+      { property: "og:image", content: "/img/dev-docs-share-img.png" },
+      { property: "og:image:width", content: "1200" },
+      { property: "og:image:height", content: "627" },
+      { property: "og:image:type", content: "image/png" },
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:title", content: "Unlock Protocol Technical Docs" },
+      {
+        name: "twitter:description",
+        content:
+          "Unlock Protocol technical documentation for developers with a complete protocol reference, tutorials and code examples.",
       },
+      { name: "twitter:image", content: "/img/dev-docs-share-img.png" },
+      {
+        name: "twitter:image:alt",
+        content: "Unlock logo with the word docs next to it",
+      },
+    ],
+    algolia: {
+      appId: "J4FN2FD27Q",
+      apiKey: "9bcefa2858ec26676689edd55f03fd26",
+      indexName: "unlock-protocol",
+      contextualSearch: true,
     },
+    navbar: {
+      title: "Unlock",
+      logo: {
+        alt: "Unlock Protocol",
+        src: "img/logo.svg",
+        href: "https://unlock-protocol.com/",
+      },
+      items: [
+        { to: "/", label: "Docs", position: "right" },
+        {
+          to: "https://app.unlock-protocol.com/dashboard",
+          label: "Dashboard",
+          position: "right",
+        },
+        { to: "/core-protocol", label: "Core Protocol", position: "right" },
+        { to: "/governance", label: "Governance", position: "right" },
+        {
+          href: "https://github.com/unlock-protocol/unlock",
+          label: "GitHub",
+          position: "right",
+        },
+        {
+          to: "https://unlock-protocol.gitbook.io/",
+          label: "Older Docs",
+          position: "right",
+          rel: "nofollow,noindex",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Overview",
+              to: "/",
+            },
+            {
+              label: "Tools",
+              to: "/",
+            },
+            {
+              label: "Tutorials",
+              to: "/",
+            },
+            {
+              label: "Goverance",
+              to: "/",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/unlock-protocol/unlock",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.com/invite/Ah6ZEJyTDp",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/UnlockProtocol",
+            },
+            {
+              label: "Forum",
+              href: "https://unlock.community/",
+            },
+          ],
+        },
+        {
+          title: "About Unlock",
+          items: [
+            {
+              label: "About Unlock",
+              to: "https://unlock-protocol.com",
+            },
+            {
+              label: "Blog",
+              to: "https://app.unlock-protocol.com",
+            },
+            {
+              label: "Guides",
+              to: "https://unlock-protocol.com/guides",
+            },
+            {
+              label: "Brand kit",
+              to: "https://unlock-protocol.com/guides/#",
+            },
+          ],
+        },
+        {
+          title: "Unlock apps",
+          items: [
+            {
+              label: "Launch dashboard",
+              to: "https://app.unlock-protocol.com",
+            },
+            {
+              label: "Grants for developer",
+              to: "https://unlock-protocol.com/grants",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Unlock, Inc.`,
+    },
+    prism: {
+      additionalLanguages: ["solidity"],
+      theme: UnlockPrismTheme,
+    },
+  },
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  }
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 };
 
 module.exports = config;
